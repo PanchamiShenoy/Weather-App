d0b5d972ef9b0eebbdc3a298008e5a8b421add88
PanchamiShenoy <61827955+PanchamiShenoy@users.noreply.github.com>
2023-04-10 18:48:50 -0400

    Update SignInViewController.swift

--- WeatherApp/ViewControllers/SignInViewController.swift
+++ WeatherApp/ViewControllers/SignInViewController.swift
@@ -79,8 +79,10 @@ class SignInViewController: UIViewController {
     }
     
     func transitionToHome() {
-        guard let homeViewController = storyboard?.instantiateViewController(withIdentifier:"homeVC") else { return  }
-        navigationController?.pushViewController(homeViewController, animated: true)
+        let vc = self.storyboard?.instantiateViewController(withIdentifier: "homeVC") as! HomeViewController
+                let navigationController = UINavigationController(rootViewController: vc)
+            navigationController.modalPresentationStyle = .fullScreen
+        self.present(navigationController, animated: true, completion: nil)
     }
     
     func showAlert(title: String, message: String) {
